29e345d837fbdf7ae78e34d8ce384c55389b1aa0
DewaldH <dewald@flatrocksolutions.co.za>
2025-02-11 17:17:32 +0200

    init

---
+++ src/test/kotlin/com/valr/valrTechassessment/ValrTechassessmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.valr.valrTechassessment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ValrTechassessmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
